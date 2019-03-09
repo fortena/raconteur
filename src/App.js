5cc612980f6a70abb6ac047b01c0f319799971aa
fortena <arnigeir@gmail.com>
2019-03-09 17:25:42 +0100

    podcast search

--- src/App.js
+++ src/App.js
@@ -1,28 +1,13 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import Home from './Home';
+import Podcast from './Podcast';
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
-  }
-}
-
-export default App;
+export default () => (
+  <BrowserRouter basename={process.env.PUBLIC_URL}>
+    <Switch>
+      <Route exact path="/" component={Home} />
+      <Route path="/podcast/:id/" component={Podcast} />
+    </Switch>
+  </BrowserRouter>
+);
