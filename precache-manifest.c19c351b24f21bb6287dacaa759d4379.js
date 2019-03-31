98069db0228883a5f3fd3abb3ce10fbe47e21661
fortena <arnigeir@gmail.com>
2019-03-31 21:52:08 +0200

    Updates

---
+++ precache-manifest.c19c351b24f21bb6287dacaa759d4379.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "26f896a0ad2c39060266",
+    "url": "/static/css/main.28d9f20a.chunk.css"
+  },
+  {
+    "revision": "26f896a0ad2c39060266",
+    "url": "/static/js/main.26f896a0.chunk.js"
+  },
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.fdfcfda2.js"
+  },
+  {
+    "revision": "e4f47b64a24226bbf9f7",
+    "url": "/static/js/2.e4f47b64.chunk.js"
+  },
+  {
+    "revision": "a2f963eaeb700e06c75e0b9f388a42c4",
+    "url": "/index.html"
+  }
+];
