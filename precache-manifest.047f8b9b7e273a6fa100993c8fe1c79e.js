e112c1a3c22b1b0a21113a3f3fb350769a9205f4
fortena <arnigeir@gmail.com>
2019-05-06 00:06:57 +0200

    Updates

---
+++ precache-manifest.047f8b9b7e273a6fa100993c8fe1c79e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "835b23505894777d396800313c53b2e1",
+    "url": "/raconteur/index.html"
+  },
+  {
+    "revision": "c14727aca1151a76d2ff",
+    "url": "/raconteur/static/css/main.795991f2.chunk.css"
+  },
+  {
+    "revision": "52966e33cc02e52f897a",
+    "url": "/raconteur/static/js/2.4fc18c04.chunk.js"
+  },
+  {
+    "revision": "c14727aca1151a76d2ff",
+    "url": "/raconteur/static/js/main.8845302a.chunk.js"
+  },
+  {
+    "revision": "856a0c7b39e96bc9777e",
+    "url": "/raconteur/static/js/runtime~main.a9e115cc.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -11,13 +11,19 @@
  * See https://goo.gl/2aRDsh
  */
 
-importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/cast-wiki/precache-manifest.9db584acadb0e20573d95b51cf457e59.js"
+  "/raconteur/precache-manifest.047f8b9b7e273a6fa100993c8fe1c79e.js"
 );
 
-workbox.clientsClaim();
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
 
 /**
  * The workboxSW.precacheAndRoute() method efficiently caches and responds to
@@ -25,10 +31,9 @@ workbox.clientsClaim();
  * See https://goo.gl/S9QRab
  */
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
-workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/cast-wiki/index.html", {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/raconteur/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
