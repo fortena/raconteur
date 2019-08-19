f093ff4d2d3159d26087047fc49f3a77a81f6a7d
fortena <arnigeir@gmail.com>
2019-08-19 23:04:58 +0200

    add more slides

--- src/Home.js
+++ src/Home.js
@@ -64,6 +64,27 @@ export default ({ history, location }) => {
             },
             {
               imageUrl:
+                'http://pics.yatpay.net/17-10-27/1200/20171027-0016.jpg',
+              title: 'Mike Duncan',
+              podcasts: [
+                {
+                  id: 'ed7b7197dba8478bae1e8ab6ea412d81',
+                  image:
+                    'https://cdn-images-1.listennotes.com/podcasts/revolutions-mike-duncan-dOrPaDkfLUo.300x300.jpg',
+                  publisher_original: 'Mike Duncan',
+                  title_original: 'Revolutions'
+                },
+                {
+                  id: '0796ebc1a6874c05a7eb3a8b5beba450',
+                  image:
+                    'https://cdn-images-1.listennotes.com/podcasts/the-history-of-rome-mike-duncan-zLe-i7FbwnA.300x300.jpg',
+                  publisher_original: 'Mike Duncan',
+                  title_original: 'The History of Rome'
+                }
+              ]
+            },
+            {
+              imageUrl:
                 'https://timedotcom.files.wordpress.com/2016/04/jad-abumrad.jpg?quality=85&w=1012&h=569&crop=1',
               title: 'Jad Abumrad',
               podcasts: [
@@ -80,6 +101,49 @@ export default ({ history, location }) => {
                     'https://cdn-images-1.listennotes.com/podcasts/radiolab-presents-more-perfect-wnyc-studios-Clw1Jjf4Bry.300x300.jpg',
                   publisher_original: 'WNYC Studios',
                   title_original: 'Radiolab Presents: More Perfect'
+                }
+              ]
+            },
+            {
+              imageUrl:
+                'https://www.sundaypost.com/wp-content/uploads/sites/13/2019/07/5d3abdaf8ee852.79749622.jpg',
+              title: 'Alex Goldman and PJ Vogt',
+              podcasts: [
+                {
+                  id: '3909a2c0c06c47d2a30422d62fe969be',
+                  image:
+                    'https://cdn-images-1.listennotes.com/podcasts/reply-all-gimlet-BNLxkVvzfKP-U8JoNCP62gM.300x300.jpg',
+                  publisher_original: 'Gimlet',
+                  title_original: 'Reply All'
+                }
+              ]
+            },
+            {
+              imageUrl:
+                'https://1omv27318zyr1hkzinzz4e4u-wpengine.netdna-ssl.com/wp-content/uploads/2014/09/malcolm-gladwell-945x540.jpg',
+              title: 'Malcolm Gladwell',
+              podcasts: [
+                {
+                  id: 'e023bce0d56d458b99775cbed7e50f3a',
+                  image:
+                    'https://cdn-images-1.listennotes.com/podcasts/revisionist-history-pushkin-industries-iCe4S7HnXP9-dnIGUict_ZQ.300x300.jpg',
+                  publisher_original: 'Pushkin Industries',
+                  title_original: 'Revisionist History'
+                },
+                {
+                  id: '1cdda6016c0143d9993d7382f69e237d',
+                  image:
+                    'https://cdn-images-1.listennotes.com/podcasts/broken-record-with-malcolm-gladwell-rick-le3I0pCMrDb.300x300.jpg',
+                  publisher_original: 'Pushkin Industries',
+                  title_original:
+                    'Broken Record with Malcolm Gladwell, Rick Rubin, and Bruce Headlam'
+                },
+                {
+                  id: '4474938df00745d79abb9759387a06f3',
+                  image:
+                    'https://cdn-images-1.listennotes.com/podcasts/solvable-pushkin-industries-O_SAFm3Gery-gZfsqC9wyxi.300x300.jpg',
+                  publisher_original: 'Pushkin Industries',
+                  title_original: 'Solvable'
                 }
               ]
             },
